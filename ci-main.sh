c7121585b5723a509fc77bc662bb9ae387ea42ac
Jeremy Busk <jeremybusk@gmail.com>
2021-09-23 11:09:28 -0600

    fix ci-main

--- ci-main.sh
+++ ci-main.sh
@@ -4,9 +4,6 @@ set -e
 echo "Style check."
 # pip install flake8 && flake8 zabbix-ldap.conf  # Not ready yet
 
-apt-get install -y gettext-base
-envsubst < zabbix-ldap.conf.envsubst > zabbix-ldap.conf
-
 echo "Build and push docker container to Dockerhub."
 release=latest
 docker build --tag uvoo/ldap-to-zabbix-sync :$release .
